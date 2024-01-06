d4aaaf5d83eab8e1063063511ee19e9089003d1f
Felipe Torres <fetl9239@ufmg.br>
2024-01-06 19:27:04 -0300

    feat: add imageData and updateDate to Masterpiece model

--- data/src/main/java/com/example/data/models/masterpiece/Masterpiece.kt
+++ data/src/main/java/com/example/data/models/masterpiece/Masterpiece.kt
@@ -6,7 +6,9 @@ data class Masterpiece(
     val id: Int,
     var name: String?,
     val imageMetadata: String?,
+    val imageData: String?,
     var description: String?,
     val creationDate: ZonedDateTime?,
+    var updateDate: ZonedDateTime?,
     val publishingDate: ZonedDateTime,
 )
