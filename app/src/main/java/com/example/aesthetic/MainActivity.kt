017f50f4256e62836b96547c73607c62386c5e9d
Felipe Torres <fetl9239@ufmg.br>
2024-01-14 18:07:27 -0300

    feat: create AestheticApp composable and apply it to MainActivity

--- app/src/main/java/com/example/aesthetic/MainActivity.kt
+++ app/src/main/java/com/example/aesthetic/MainActivity.kt
@@ -1,62 +1,19 @@
 package com.example.aesthetic
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.aesthetic.ui.theme.AestheticTheme
+import com.example.aesthetic.ui.AestheticApp
+import com.example.designsystem.theme.AestheticTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Registers a photo picker activity launcher in multi-select mode.
-        // In this example, the app lets the user select up to 5 media files.
-        val pickMultipleMedia =
-            this.registerForActivityResult(ActivityResultContracts.PickMultipleVisualMedia(5)) { uris ->
-                // Callback is invoked after the user selects media items or closes the
-                // photo picker.
-                if (uris.isNotEmpty()) {
-                    Log.d("PhotoPicker", "Number of items selected: ${uris.size}")
-                } else {
-                    Log.d("PhotoPicker", "No media selected")
-                }
-            }
-
         setContent {
             AestheticTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                AestheticApp()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AestheticTheme {
-        Greeting("Android")
     }
 }
