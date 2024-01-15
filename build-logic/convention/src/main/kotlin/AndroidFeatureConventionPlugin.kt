b803dae27268f142d9c5757d2357064478cd0c23
Felipe Torres <fetl9239@ufmg.br>
2024-01-14 23:45:42 -0300

    fix: remove implementation of :core:ui on AndroidFeatureConventionPlugin

--- build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -28,7 +28,6 @@ class AndroidFeatureConventionPlugin : Plugin<Project> {
             }
 
             dependencies {
-                add("implementation", project(":core:ui"))
                 add("implementation", project(":core:designsystem"))
 
                 add("implementation", libs.findLibrary("androidx.hilt.navigation.compose").get())
