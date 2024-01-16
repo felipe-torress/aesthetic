95bf34af95d87b6a93a7c437ceff4e61bdde56f0
Felipe Torres <fetl9239@ufmg.br>
2024-01-16 00:31:43 -0300

    feat: create TopLevelDestination - NavBar Tabs

---
+++ app/src/main/kotlin/com/example/aesthetic/navigation/TopLevelDestination.kt
@@ -0,0 +1,31 @@
+package com.example.aesthetic.navigation
+
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.example.aesthetic.feature.museum.R as museumR
+import com.example.aesthetic.feature.profile.R as profileR
+import com.example.aesthetic.core.designsystem.icon.AestheticIcons
+
+/**
+ * Type for the top level destinations in the application. Each of these destinations
+ * can contain one or more screens (based on the window size). Navigation from one screen to the
+ * next within a single destination will be handled directly in composables.
+ */
+enum class TopLevelDestination(
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val iconTextId: Int,
+    val titleTextId: Int,
+) {
+    MUSEUM(
+        selectedIcon = AestheticIcons.Add,
+        unselectedIcon = AestheticIcons.AddBorder,
+        iconTextId = museumR.string.feature_museum_title,
+        titleTextId = museumR.string.feature_museum_title,
+    ),
+    PROFILE(
+        selectedIcon = AestheticIcons.Person,
+        unselectedIcon = AestheticIcons.PersonBorder,
+        iconTextId = profileR.string.feature_profile_title,
+        titleTextId = profileR.string.feature_profile_title,
+    )
+}
