ec6dc0626de829ebd07364dfae5001ba3f4a6c75
Felipe Torres <fetl9239@ufmg.br>
2024-01-16 00:29:27 -0300

    feat: create AestheticIcons object

---
+++ core/designsystem/src/main/kotlin/com/example/aesthetic/core/designsystem/icon/AestheticIcons.kt
@@ -0,0 +1,21 @@
+package com.example.aesthetic.core.designsystem.icon
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AddCircle
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material.icons.rounded.Settings
+
+object AestheticIcons {
+    //region --- NavBar Icons ---
+    val Person = Icons.Rounded.Person
+    val PersonBorder = Icons.Outlined.Person
+    val Add = Icons.Rounded.Add
+    val AddBorder = Icons.Outlined.AddCircle
+    //endregion
+
+    //region --- Misc ---
+    val Settings = Icons.Rounded.Settings
+    //endregion
+}
