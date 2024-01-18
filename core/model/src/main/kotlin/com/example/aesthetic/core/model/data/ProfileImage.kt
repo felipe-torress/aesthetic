3a750723a29b5fa5659678dcb2b9ed49764fae42
Felipe Torres <fetl9239@ufmg.br>
2024-01-17 23:52:52 -0300

    feat: add imageId and artistId to ProfileImage data class

--- core/model/src/main/kotlin/com/example/aesthetic/core/model/data/ProfileImage.kt
+++ core/model/src/main/kotlin/com/example/aesthetic/core/model/data/ProfileImage.kt
@@ -3,6 +3,8 @@ package com.example.aesthetic.core.model.data
 import java.time.ZonedDateTime
 
 data class ProfileImage(
+    val id: String,
+    val artistId: String,
     val imageData: String,
     val updateDateTime: ZonedDateTime
 )
